2a1e3527b55f423b5ce7ae3e5b0b20d0696848a1
bcgood98 <bengood98@gmail.com>
2021-05-18 11:29:41 -0800

    First commit

---
+++ game_of_life.py
@@ -0,0 +1,125 @@
+import random, inspect
+import tkinter as tk
+
+init_geno = ["0"] * 60
+
+class Node:
+	
+	# Node should contain either a value or a function
+	def __init__(self, func = None, val = None, children = [], parent = None):
+		if func is not None and val is not None:
+			raise "Node passed function and value"
+			
+		if func is not None:
+			self.func = func
+			argtup = inspect.getargspec(func)
+			self.arity = len(argtup[args])
+			self.type = 1
+		
+		if val is not None:
+			self.type = 0
+			self.value = val
+		
+		self.children = children
+		self.parent = parent
+	
+	# if the node contains a function, run it
+	def run(self):
+		if self.func is not None:
+			return self.func()
+	
+	def add_child(self, child):
+		self.children.append(child)
+			
+class Brain:
+	
+	def __int__(self, root):
+		self.root = root
+
+class Individual:
+	
+	# initialize the genotype, memories, and brain
+	def __init__(self, brain, geno):
+		self.genotype = geno
+		self.memories = []
+		self.brain = brain
+	
+	def mutation(self):
+		for g in self.genotype:
+			pm = random.randint(1, len(self.genotype)) # The probability of mutation is 1/n, with n being the length of the genome
+			if pm == 2:
+				if g == "0":
+					g = "1"
+				else:
+					g = "0"
+	
+	
+
+# takes two strings and the probability of crossover
+def sp_crossover(s1, s2, prob):
+	if len(s1) > 2 and random.rand() > prob:
+		point = random.randint(1, len(s1) - 1)
+		child = s1[0:point] + s2[point:]
+		return child
+
+#def mutation(geno):
+#	for g in geno:
+#		pm = random.randint(1, len(geno)) # The probability of mutation is 1/n, with n being the length of the genome
+#		if pm == 2:
+#			if g == "0":
+#				g = "1"
+#			else:
+#				g = "0"
+	
+#	return geno
+
+# check if cell is occupied
+def occupied(x, y, curmap):
+	if curmap[x][y] is None:
+		return False
+	return True
+
+# find an open cell
+def findpos(curmap, width, height):
+	x = random.randint(0, width-1)
+	y = random.randint(0, height-1)
+	if not occupied(x, y, curmap):
+		return x, y
+	else:
+		return findpos(curmap, width, height)
+#	for y in range(height):
+#		if not occupied(x, y):
+#			return x, y
+
+# generate the initial population, including assignments to cells
+def gen_initpop(size, width, height, curmap):
+	#orgmap = []
+	for i in range(size):
+		x, y = findpos(curmap, width, height)
+		curmap[x][y] = Individual(Brain(), init_geno)
+	return curmap
+	
+if __name__ == "__main__":
+
+	def paint_cells(curmap, canvas, width, height):
+		for x in range(width):
+			for y in range(height):
+				if occupied(x, y, curmap):
+					canvas.create_rectangle(x, y, x+1, y+1, fill="blue")
+	
+	map_width = 1000
+	map_height = 800
+	map = [None] * map_width
+	for x in range(map_width):
+		map[x] = [None] * map_height
+	map = gen_initpop(100, map_width, map_height, map)
+	root = tk.Tk()
+	can = tk.Canvas(root, bg="white", height=800, width=1000)
+	can.pack()
+	paint_cells(map, can, map_width, map_height)
+#	for x in range(map_width):
+#			for y in range(map_height):
+#				if occupied(x, y, map):
+#					print("occupied")
+#					can.create_rectangle(x, y, x+4, y+4, fill="red")
+	root.mainloop()
---
+++ primitive_functions.py
@@ -0,0 +1,3 @@
+# conns stores which functions can be called by a given function/node
+conns = {}
+conns['root'] = []
